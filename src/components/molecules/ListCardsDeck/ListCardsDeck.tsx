92756e15c521720eec8d05f4b9c0969659ab30c2
Lucas Henrique <lukas.paes18@gmail.com>
2023-03-10 03:57:08 -0300

    refactor list

--- src/components/molecules/ListCardsDeck/ListCardsDeck.tsx
+++ src/components/molecules/ListCardsDeck/ListCardsDeck.tsx
@@ -3,6 +3,8 @@ import {useDispatch} from 'react-redux';
 import {useAppSelector} from '../../../hooks';
 import {readDeckRequest} from '../../../store/reducer/deck/actions';
 
+import {Container} from './style';
+
 export const ListCardsDeck = () => {
   const {loading, cards} = useAppSelector((state) => state.deck);
 
@@ -17,20 +19,13 @@ export const ListCardsDeck = () => {
       {loading ? (
         <></>
       ) : (
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'center',
-            gap: '10px',
-          }}
-        >
+        <Container>
           {cards.map((item: any) => {
             return (
               <img src={item.image} style={{width: '160px', height: '200px'}} />
             );
           })}
-        </div>
+        </Container>
       )}
     </>
   );
